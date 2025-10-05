dbb79cd817c91d7b553bb5115f8fb3fea18abba0
Kenneth McClure <73478341+kmcclure27@users.noreply.github.com>
2025-10-05 14:59:33 -0600

    Added MZI-GRM

--- R/MZIGRM_tools.R
+++ R/MZIGRM_tools.R
@@ -1,47 +1,17 @@
-#### Multidimensional Hurdle Graded Response Model Functions ####
 
-#' GRM Item Response Probabilities
-#'
-#' Calculates the Graded Response Model response category probabilities
-#'
-#' @param theta An N x M matrix of person parameters
-#' @param slope A 1 x M matrix of item slopes
-#' @param intercept A 1 x K-1 matrix of item intercepts
-#' @param D A scaling Constant
-#'
-#' @return An N x K matrix of item response probabilities
-#'
-GRM_mat2 <- function(theta,slope,intercept,D=1.702){
-  if(is.vector(theta)){
-    theta=matrix(theta,nrow=1)
-  }
-  N = nrow(theta)
-  n_ints = length(intercept)
-  eta <- matrix(-D*theta%*%t(slope),nrow=N,
-                ncol=n_ints)
-  int_mat = matrix(as.numeric(intercept),
-                   nrow=N,ncol=n_ints,
-                   byrow=T)
-  y = eta + int_mat
-  cum_prob = cbind(rep(1,N),
-                   (1+exp(y))^-1,
-                   rep(0,N))
-  p = cum_prob[,1:(n_ints+1)] - cum_prob[,2:(n_ints+2)]
-  return(p)
-}
 
-#' MHGRM Response Probabilities
+#' MZIGRM Response Probabilities
 #'
-#' Calculates the MH Graded Response Model response category probabilities
+#' Calculates the MZI Graded Response Model response category probabilities
 #'
 #' @param theta An N x 2 Matrix of Person Parameters
 #' @param slope A 1 x 2 Matrix of item slopes
-#' @param intercept A 1 x K-1 Matrix of Item Intercepts
+#' @param intercept A 1 x K Matrix of Item Intercepts. The first intercept corresponds to the 2PL component
 #' @param D A scaling Constant
 #'
 #' @return An N x K matrix of response probabilities
 #'
-MHGRM_mat2 <- function(theta,slope,intercept,D=1.702){
+MZIGRM_mat2 <- function(theta,slope,intercept,D=1.702){
   if(is.vector(theta)){
     theta=matrix(theta,nrow=1)
   }
@@ -53,39 +23,32 @@ MHGRM_mat2 <- function(theta,slope,intercept,D=1.702){
   P_GRM = GRM_mat2(theta=theta[,2,drop=F],
                    slope=slope[,2,drop=F],
                    intercept=intercept[,2:n_ints,drop=F],D=D)
-  Pk = cbind(P0[,1],
-             P_GRM*P0[,2])
+  #print(P_GRM)
+  Pk = cbind(P0[,1]+(P0[,2])*P_GRM[,1],
+             (P0[,2]*P_GRM[,2:n_ints]))
   return(Pk)
 }
 
-#' Simulate Item Responses from MHGRM
+#' Simulate Item Responses for the MZIGRM
 #'
-#' Simulates item responses under the MHGRM give an N x K matrix of response probabilities
 #'
-#' @param x An N x K matrix of response probabilities
-#' @param seed A numeric value to set the random seed
 #'
-#' @return An N length vector of item responses ranging from 0:(K-1)
+#' @inheritParams MHGRM_simulate_response
 #'
-MHGRM_simulate_response <- function(x,seed=NULL){
-  if(!is.null(seed)){
-    stopifnot("seed must be numeric"=is.numeric(seed))
-    set.seed(seed)
-  }
-  y = apply(x,1,function(x){
-    sample(1:length(x),size=1,prob=x)-1
-  })
+#' @return An N length vector of item responses 0:(K-1)
+#'
+MZIGRM_simulate_response <- function(x,seed=NULL){
+  y = MHGRM_simulate_response(x=x,seed=seed)
   return(y)
 }
 
-
-#' Generate MHGRM Data
+#' Generate MZIGRM Data
 #'
-#' Generates Some Factory MHGRM Item Parameters, Person Parameters, and Item Responses
+#' Generates Some Factory MZIGRM Item Parameters, Person Parameters, and Item Responses
 #'
 #' @param N Number of Observations
 #' @param J Number of Items
-#' @param K Number of Response Categories. Must be greater than 2 for the MHGRM
+#' @param K Number of Response Categories. Must be greater than 1 for the MZIGRM
 #' @param rho Correlation Between Person Parameters
 #' @param seed_person_params An optional numeric to set the seed for person parameters
 #' @param seed_item_params An optional numeric to set the seed for item parameters
@@ -93,19 +56,16 @@ MHGRM_simulate_response <- function(x,seed=NULL){
 #'
 #' @return A list containing the Generated Person Parameters, Item Parameters, and simulate item responses
 #' @export
-#'
-generate_MHGRM_data <- function(N,J,K,rho=0,
+generate_MZIGRM_data <- function(N,J,K,rho=0,
                                 seed_person_params=NULL,seed_item_params=NULL,
                                 seed_response=NULL){
-  stopifnot("K must be greater than 2" = K>2,
+  stopifnot("K must be greater than 1" = K>1,
             "K must be an integer" = K%%1==0,
             "N must be an integer" = N%%1==0,
             "J must be an integer" = J%%1==0,
             "rho must be numeric between -1 and 1"=all(is.numeric(rho),
                                                        length(rho)==1,
                                                        abs(rho)<1))
-
-
   if(!is.null(seed_person_params)){
     stopifnot("seed_person_params should be a scalar"=length(seed_person_params)==1,
               "seed_person_params must be numeric"=is.numeric(seed_person_params))
@@ -119,20 +79,21 @@ generate_MHGRM_data <- function(N,J,K,rho=0,
   }
   a <- matrix(stats::runif(n=2*J,min=1,max=4),nrow=J,ncol=2)
   intercept0 <- matrix(stats::runif(n=J,min=-2.5,max=0),nrow=J) #b0
-  interceptK <- matrix(stats::runif(n = J*(K-2),min = -2.5, max = 2.5),
+  interceptK <- matrix(stats::runif(n = J*(K-1),min = -2.5, max = 2.5),
                        nrow=J)
-  if(K==3){
+  if(K==2){
     b = cbind(intercept0,interceptK)
   }else{
     interceptK <- t(apply(interceptK,1,sort))
+    b <- cbind(intercept0,interceptK)
   }
-
-  b <- cbind(intercept0,interceptK)
+  #print(dim(b))
+  #return(b)
   item_params = list(slopes=a,intercepts=b)
 
   resp_ps <- array(data=NA, dim = c(N,K,J))
   for(j in 1:J){
-    resp_ps[,,j] <- MHGRM_mat2(theta=pps,
+    resp_ps[,,j] <- MZIGRM_mat2(theta=pps,
                                slope=a[j,,drop=F],
                                intercept=b[j,,drop=F])
   }
@@ -142,24 +103,24 @@ generate_MHGRM_data <- function(N,J,K,rho=0,
       stopifnot("seed_response must be numeric"=is.numeric(seed_response))
       set.seed(seed_response)
       for(j in 1:J){
-        item_resps[,j] <- MHGRM_simulate_response(x=resp_ps[,,j])
+        item_resps[,j] <- MZIGRM_simulate_response(x=resp_ps[,,j])
       }
     }else if(length(seed_response)==J){
       stopifnot("seed_response must be numeric"=is.numeric(seed_response))
       for(j in 1:J){
-        item_resps[,j] <- MHGRM_simulate_response(x=resp_ps[,,j],
+        item_resps[,j] <- MZIGRM_simulate_response(x=resp_ps[,,j],
                                                   seed=seed_response[j])
       }
     }else{
       warning("seed_response must be a scalar or J length numeric vector. seed not set")
       for(j in 1:J){
-        item_resps[,j] <- MHGRM_simulate_response(x=resp_ps[,,j],
+        item_resps[,j] <- MZIGRM_simulate_response(x=resp_ps[,,j],
                                                   seed=seed_response[j])
       }
     }
   }else{
     for(j in 1:J){
-      item_resps[,j] <- MHGRM_simulate_response(x=resp_ps[,,j])
+      item_resps[,j] <- MZIGRM_simulate_response(x=resp_ps[,,j])
     }
   }
   colnames(item_resps) <- paste0("x",1:J)
--- R/create_mirt_objects.R
+++ R/create_mirt_objects.R
@@ -1,11 +1,11 @@
 ### Attempt at a General Constructor function for an MHGRM mirt item type
 
-#' Create a Multidimensional Hurdle Graded Response Model mirt Item
+#' Create a Multidimensional Hurdle Graded Response Model mirt custom item
 #'
 #' Creates a Multidimensional Hurdle Graded Response Model (MHGRM) with K response categories as a custom item in the mirt package
 #' @param K An integer denoting the number of item response categories
 #'
-#' @return A mirt item from mirt::createItem()
+#' @return A mirt item from mirt::createItem() named "MHGRM_K"
 #' @export
 #'
 create_mirt_MHGRM <- function(K){
@@ -39,3 +39,39 @@ create_mirt_MHGRM <- function(K){
   return(x)
 }
 
+#' Create A Multidimesnional Zero-Inflated Graded Response Model mirt custom item
+#'
+#' NOT TESTED YET
+#'
+#' @param K Number of Item Response Categories (integer)
+#'
+#' @return A mirt custom item from mirt::createItem() named "MZIGRM_K"
+#'
+create_mirt_MZIGRM <- function(K){
+  stopifnot("K must be an integer"=all(is.numeric(K),K))
+  name=paste0("MZIGRM_",K)
+  par = rep(a=c(1,2),b=seq(-1,1,length.out=K))
+  est = rep(TRUE,length(par))
+  P.MZIGRM_K <- function(par,Theta,ncat){
+    D = 1.702 # Scaling Constant
+    a1 = par[1]
+    a2 = par[2]
+    b0 = par[3]
+    b = par[-c(1,2,3)]
+    P0 = 1 - 1/(1+exp(-D*a1*Theta[,1]+b0))
+    CP = matrix(0,nrow=nrow(Theta),ncol=length(b))
+    for(k in 1:length(b)){
+      CP[,k] <- 1/(1+exp(-D*a2*Theta[,2,drop=F]+b[k]))
+    }
+    CP = cbind(rep(1,nrow(CP)),
+               CP,
+               rep(0,nrow(CP)))
+    P_GRM = CP[,1:ncat,drop=F]-CP[,2:(ncat+1),drop+F]
+    Pk = cbind((P0+(1-P0)*P_GRM[,1,drop=F]),
+              (1-P0)*P_GRM[,2:(ncat)+1,drop=F])
+    colnames(Pk) <- NULL
+    return(Pk)
+  }
+  x <- mirt::createItem(name=name,par=par,est=est,P=P.MZIGRM_K)
+  return(x)
+}
