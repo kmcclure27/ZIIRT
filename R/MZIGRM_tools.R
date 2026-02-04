db05caadc17686d04a87411c4eef7eabae1174db
Kenneth McClure <73478341+kmcclure27@users.noreply.github.com>
2026-02-04 13:00:55 -0700

    updated doc

--- R/MZIGRM_tools.R
+++ R/MZIGRM_tools.R
@@ -161,7 +161,6 @@ generate_MZIGRM_data <- function(N,J,K,rho=0,theta=NULL,a=NULL,b=NULL,
   return(out)
 }
 
-
 #' Calculates Expected Proportion of Zero Inflation
 #'
 #' Calculates the expected proportion of zeros due to zero inflation as a function of the presence latent variable. Assumes theta is standard bivariate normal.
@@ -173,7 +172,6 @@ generate_MZIGRM_data <- function(N,J,K,rho=0,theta=NULL,a=NULL,b=NULL,
 #'
 #' @return The expected proportion of zeros in the MZI-GRM
 #' @export
-
 MZI_propZI <- function(b0,b1,theta=c(0,0),a=c(1,1)){
   stopifnot("b0 and b1 must be numeric"=all(is.numeric(b0),is.numeric(b1)),
             "theta must be length 2"=length(theta)==2,
@@ -201,7 +199,6 @@ MZI_propZI <- function(b0,b1,theta=c(0,0),a=c(1,1)){
 #'
 #' @return A n-row dataframe with columns for 1) expected proportion of zero inflated due to low presence, 2) b0, and 3) b1
 #' @export
-
 MZI_find_b0 <- function(propZI0,b1,n=1,tol=0.10,thr=NULL){
   ### ERROR HANDLING HERE ###
   stopifnot("b1 must be numeric"=is.numeric(b1),
@@ -239,57 +236,6 @@ MZI_find_b0 <- function(propZI0,b1,n=1,tol=0.10,thr=NULL){
   return(b0)
 }
 
-#' Find b0 for MZI given a target Zero-Inflation Level
-#'
-#' Finds item intercept(s) for the presence latent variable in the MZI given target level of zero-inflation due to (lack of) presence and the item intercept controlling zeros from low severity
-#' Assumes that the presence/severity latent variables are standard bivariate normal.
-#'
-#' @param propZI0 The target proportion of zeros due to (lack of) presence. Must be a decimal between 0 and 1.
-#' @param b1 Item Intercept controlling potential zeros from the severity component of the MZIGRM
-#' @param n The number of candidate b0 to output. The n that provide the closest propZI0 are returned
-#' @param tol A tolerance threshold that issues a warning if the resulting proportion of zero inflation is more than tol from the target
-#' @param thr An alternative to n that outputs all b0 parameters with thr of the target; defaults to NULL
-#'
-#' @return A n-row dataframe with columns for 1) expected proportion of zero inflated due to low presence, 2) b0, and 3) b1
-#' @export
-
-MZI_find_b0 <- function(propZI0,b1,n=1,tol=0.10,thr=NULL){
-  ### ERROR HANDLING  ###
-  stopifnot("b1 must be numeric"=is.numeric(b1),
-            "propZI0 must be numeric"=is.numeric(propZI0),
-            "propZI0 must be positive"=propZI0>0,
-            "propZI0 must be less than 1"=propZI0<1,
-            "n must be an integer"=n%%1==0,
-            "tol must be numeric"=is.numeric(tol),
-            "tol must be less than 1"=tol<1)
-
-  if(!is.null(thr)){
-    stopifnot("thr must be numeric"=is.numeric(thr),
-              "thr must be less than 1"=abs(thr)<1)
-  }
-  ###########################
-  x = propZI_table #internal data object
-  x$d = abs(propZI_table$propZI-propZI0)
-  x_sub = x[x$b1==b1,] #need to handle when b0 isn't exactly in the
-  if(nrow(x_sub)==0){
-    y = x$b1[which.min(abs(x$b1-b1))]
-    x_sub = x[x$b1==y,]
-   # warning(paste0("No perfect match for b1 = ",b1," found. b1 = ",y," was used"))
-  }
-  o = order(x_sub$d)
-  x_sub = x_sub[o,];rownames(x_sub) <- NULL
-  if(is.numeric(thr)){
-    n = sum(x_sub$d<thr)
-    message(paste0(n," possibilities within ",thr," of propZI = ",propZI0))
-  }
-  b0 = x_sub[1:n,c("b0","b1","propZI")]
-  if(abs(x_sub$propZI[1]-propZI0)>tol){
-    warning(paste0("There is a large difference between the target proportion ZI: ",propZI0,"
-                    and the optimal solution: ", round(x_sub$propZI[1],4),". Proceed with caution."))
-  }
-  return(b0)
-}
-
 #' Find b1 for MZI given a target Zero-Inflation Level
 #'
 #' Finds item intercept(s) for the severity latent variable in the MZI given target level of zero-inflation due to (lack of) presence and the item intercept controlling zeros from low presence
@@ -303,7 +249,6 @@ MZI_find_b0 <- function(propZI0,b1,n=1,tol=0.10,thr=NULL){
 #'
 #' @return A n-row dataframe with columns for 1) expected proportion of zero inflated due to low presence, 2) b0, and 3) b1
 #' @export
-
 MZI_find_b1 <- function(propZI0,b0,n=1,tol=0.1,thr=NULL){
   ### ERROR HANDLING  ###
   stopifnot("b0 must be numeric"=is.numeric(b0),
